a9f45bb4829352adc01dc78e10edcd7549bfdb24
Ed Marriott <edeaze85@gmail.com>
2019-01-18 16:06:45 +1100

    add fullscreen button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreen = player.querySelector('.fullscreen');
 
 /* build functions */
 function togglePlay() {
@@ -36,6 +37,10 @@ function scrub(e) {
 	video.currentTime = scrubTime;
 }
 
+function toggleFullscreen() {
+	document.fullscreenElement ? document.exitFullscreen() : player.requestFullscreen();
+}
+
 /* hook up event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -54,3 +59,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+fullscreen.addEventListener('click', toggleFullscreen);
